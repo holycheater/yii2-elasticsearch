9544fcb1a6b63b78a61deac79e2fab07b0d35c85
Alex Salt <alex.salt@e96.ru>
2016-12-12 22:20:58 +0500

    tests: update customer mapping

--- tests/data/ar/Customer.php
+++ tests/data/ar/Customer.php
@@ -76,13 +76,11 @@ class Customer extends ActiveRecord
     {
         $command->deleteMapping(static::index(), static::type());
         $command->setMapping(static::index(), static::type(), [
-            static::type() => [
-                "properties" => [
-                    "name" =>        ["type" => "string", "index" => "not_analyzed", "store" => true],
-                    "email" =>       ["type" => "string", "index" => "not_analyzed" ],
-                    "address" =>     ["type" => "string", "index" => "analyzed"],
-                    "status" => ["type" => "integer"],
-                ]
+            "properties" => [
+                "name" => ["type" => "keyword", "index" => "not_analyzed", "store" => true],
+                "email" => ["type" => "keyword", "index" => "not_analyzed", "store" => true],
+                "address" => ["type" => "text", "index" => "analyzed"],
+                "status" => ["type" => "integer"],
             ]
         ]);
 
